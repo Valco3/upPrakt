b19ed805aec2f438f7d7bcf47898ad45b7477fff
Valentin Velchev <92169026+Valco3@users.noreply.github.com>
2021-11-04 14:00:09 +0200

    Add files via upload

---
+++ exercise4.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //task 1
+    // int n;
+    // do
+    // {
+    //     cin>>n;
+    // }while(n<10||n>99);
+    // cout<<n;
+
+
+    // while(1)cout<<"sheesh";
+    // return 0;
+
+    //task2
+
+    //а)
+    // int n,m;
+    // cin>>n>>m;
+    // for(int i=0;i<n;i++)
+    // {
+    //     for(int i=0;i<m;i++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //б)
+    // int n;
+    // cin>>n;
+    // for(int i=0;i<n;i++)
+    // {
+    //     for(int j=0;j<i+1;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=n-1;i>=1;i--)
+    // {
+    //     for(int j=0;j<i;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //task 3
+    // int n;
+    // cin>>n;
+    // int sum=1;
+    // for(int i=n;i>0;i--)
+    // {
+    //     sum*=i;
+        
+    // }
+    // cout<<sum;
+
+    //task 4
+    // int sum=0;
+    // for(int i=100;i<1000;i++)
+    // {
+    //     int a=i/100;
+    //     int b=(i/10)%10;
+    //     int c=i%10;
+    //     if(a!=b&&a!=c&&b!=c)cout<<i<<" ";
+    //     sum++;
+    // }
+    // cout<<"Sum: "<<sum;
+
+    //task 5
+    // int sum=1;
+    // for(int i=100;i<1000;i++)
+    // {
+    //    if(i%10!=0) sum*=i%10;
+    //    if((i/10)%10!=0)sum*=(i/10)%10;
+    //    if((i/100)!=0)sum*=i/100;
+
+    //    if(i%sum==0)cout<<i<<endl;
+    //    sum=1;
+    // }
+   
+    // return 0;
+}
